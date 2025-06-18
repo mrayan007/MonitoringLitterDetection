4877e0b662e8d497dbc5399c2f376180d6880112
mrayan007 <rayanwmv@gmail.com>
2025-06-18 20:12:51 +0200

    Checkpoint.

--- Frontend/dashboardScript.js
+++ Frontend/dashboardScript.js
@@ -1,5 +1,5 @@
 const map = L.map('map', {
-  center: [51.5, -0.09],
+  center: [51.585719, 4.793235],
   zoom: 13,
   zoomControl: false
 });
@@ -8,15 +8,17 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; OpenStreetMap, Carto'
 }).addTo(map);
 
-L.marker([51.5, -0.09], {
+L.marker([51.585719, 4.793235], {
   icon: L.icon({
     iconUrl: 'Art/trashCan.png',
     iconSize: [40, 40],
     className: 'mapIcon'
   })
-}).addTo(map);
+}).addTo(map)
+  .bindPopup('Avans Hogeschool')
+  .openPopup();
 
-L.circle([51.5, -0.09], {
+L.circle([51.585719, 4.793235], {
   radius: 300,
   color: 'red',
   fillColor: '#f03',
